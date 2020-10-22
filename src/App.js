72d36e42642cd08e72fc3e0883ecfdc48b725c12
Barbara Kowalska <kowalskab389@gmail.com>
2020-10-22 21:31:04 +0200

    Forms

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import StandardForm from "./Standard-Form/StandardForm";
+import ReactHookForm from "./React-Hook-Form/ReactHookForm";
 
 function App() {
-  return (
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="container">
+            <div className="row mt-5" style={{display: 'flex', justifyContent: 'space-around' }}>
+                <div className="col-md-5 border p-5" style={{backgroundColor: 'pink'}}>
+                    <StandardForm />
+                </div>
+                <div className="col-md-5 border p-5" style={{backgroundColor: 'rosybrown'}}>
+                    <ReactHookForm />
+                </div>
+            </div>
+        </div>
+
     </div>
   );
 }
